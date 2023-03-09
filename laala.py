96d49a463298a236d0b58ae866b1cd056dd01fac
LuceoEtzio <40282884+LuceoEtzio@users.noreply.github.com>
2023-03-08 23:09:39 -0600

    Add basic token count to history

--- laala.py
+++ laala.py
@@ -1,6 +1,18 @@
 import openai
+from transformers import GPT2TokenizerFast
 from colorama import init, Fore, Back, Style
 init()
+
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+def count_tokens(payload):
+    tokend = tokenizer.encode(payload)
+    num_tokens = len(tokend)
+    return num_tokens
+
+def strip_count(message_history : tuple) -> list:
+    stripped_history = [i[0] for i in message_history]
+    return stripped_history
 
 with open('api.key', 'r') as file:
     priTicket = file.read().strip()
@@ -18,13 +30,15 @@ chatHistory = []
 
 print("## LAALA ONLINE c: ##\n")
 
-messages = [
-                {"role": "user", "content": system_desu},
+message_history = [
+                ({"role": "user", "content": system_desu}, 4),
             ]
+
+messages = strip_count(message_history)
 
 firstMessage = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-		messages=messages
+		messages = strip_count(message_history)
     )
 print(Fore.LIGHTRED_EX + "LAALA: Beep Boop~ LAALA Here~ " + firstMessage.choices[0].message.content.lstrip('\n') + Style.RESET_ALL)
 
@@ -34,15 +48,18 @@ while True:
     # Send the prompt to the OpenAI API
     print("")
     prompt = input(Fore.CYAN + "You: ")
-    messages.append({"role": "user", "content": prompt})
+    prompt_tokens = count_tokens(prompt)
+    message_history.append(tuple([{"role": "user", "content": prompt}, prompt_tokens]))
+    messages = strip_count(message_history)
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-		messages=messages
+		messages = messages
     )
     
     rawMessage = completion.choices[0]
-    messages.append(rawMessage.message)
     message = rawMessage.message.content.lstrip('\n')
+    message_tokens = count_tokens(message)
+    message_history.append(tuple([rawMessage.message, message_tokens]))
     print("")
     print(Fore.LIGHTRED_EX + "LAALA: " + message + Style.RESET_ALL)
     
