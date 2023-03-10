90bb531a8fd495782473a8e8aa1646d3ad5b5171
LuceoEtzio <40282884+LuceoEtzio@users.noreply.github.com>
2023-03-09 22:54:45 -0600

    snakeCase function names

--- laala.py
+++ laala.py
@@ -28,7 +28,7 @@ class MessageHistoryStore:
         self.userDictionary = {"role": "user", "content": prompt}
         
         #prompt -> tokens
-        self.userTokens = count_tokens(prompt)
+        self.userTokens = countTokens(prompt)
         
         #takes prompt, formats to correct ({"role": "user", "content": prompt}, 1)
         self.message_history.append(tuple(self.userDictionary, self.userTokens))
@@ -36,7 +36,7 @@ class MessageHistoryStore:
         
     def newRespEntry(self, responseDict):
         #responseDict is {"role": "assistant", "content": "bingle"}
-        self.respTokens = count_tokens(responseDict.content)
+        self.respTokens = countTokens(responseDict.content)
         self.message_history.append(tuple(responseDict.content, self.respTokens))
         
     def sendQuery(self, formatedListOfDicts):
@@ -64,18 +64,18 @@ start of loop
 go to start
 
 # count tokens of message content
-def count_tokens(payload : str) -> int:
+def countTokens(payload : str) -> int:
     tokend = tokenizer.encode(payload)
     num_tokens = len(tokend)
     return num_tokens
 
 # strip token count from history tuple, return api responses only
-def strip_count(message_history : tuple) -> list:
+def stripCount(message_history : tuple) -> list:
     stripped_history = [i[0] for i in message_history]
     return stripped_history
 
 # pop messages exceeding the user-defined history context size
-def pop_history(history_token_size : int) -> int:
+def popHistory(history_token_size : int) -> int:
     while history_token_size > max_history_size:
         history_token_size -= message_history[0][1]
         message_history.pop(0)
@@ -87,7 +87,7 @@ def inputYourPrompt() -> str:
     return prompt
 
 # append sent or recieved message to message history
-def append_history_to_send(message_history : list, prompt : str, prompt_tokens : int, append_type : str) -> list:
+def appendHistoryToSend(message_history : list, prompt : str, prompt_tokens : int, append_type : str) -> list:
     if append_type == 'prompt':
         message_history.append(tuple([{"role": "user", "content": prompt}, prompt_tokens]))
     elif append_type == 'response':
@@ -104,11 +104,11 @@ openai.api_key = str(priTicket)
 if "boring" in sys.argv:
     with open('boring_mode.txt', 'r') as file:
         system_desu = file.read().strip()
-        system_desu_count = count_tokens(system_desu)
+        system_desu_count = countTokens(system_desu)
 else:
     with open('laala_prompt.txt', 'r') as file:
         system_desu = file.read().strip()
-        system_desu_count = count_tokens(system_desu)
+        system_desu_count = countTokens(system_desu)
 
 def debugMode(history_token_size : int) -> None:
     if "debug" in sys.argv:
@@ -127,14 +127,14 @@ message_history =[(
         ),
     ]
 
-messages = strip_count(message_history)
+messages = stripCount(message_history)
 
 #4000 = sum(500,500,500,500)
 history_token_size = sum(t[1] for t in message_history)
 
 firstMessage = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-		messages = strip_count(message_history)
+		messages = stripCount(message_history)
     )
 print(Fore.LIGHTRED_EX + "LAALA: Beep Boop~ LAALA Here~ " + firstMessage.choices[0].message.content.lstrip('\n') + Style.RESET_ALL)
 
@@ -145,14 +145,14 @@ while True:
     print("")
     prompt = inputYourPrompt()
     
-    prompt_tokens = count_tokens(prompt)
+    prompt_tokens = countTokens(prompt)
     
     #message_history.append(tuple([{"role": "user", "content": prompt}, prompt_tokens]))
-    append_history_to_send(message_history, prompt, prompt_tokens, 'prompt')
+    appendHistoryToSend(message_history, prompt, prompt_tokens, 'prompt')
 
     history_token_size += prompt_tokens
-    history_token_size = pop_history(history_token_size)
-    messages = strip_count(message_history)
+    history_token_size = popHistory(history_token_size)
+    messages = stripCount(message_history)
     
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -161,11 +161,11 @@ while True:
     
     rawMessage = completion.choices[0]
     message = rawMessage.message.content.lstrip('\n')
-    message_tokens = count_tokens(message)
+    message_tokens = countTokens(message)
     history_token_size += message_tokens
     #message_history.append(tuple([rawMessage.message, message_tokens]))
     #append_history_to_send(message_history, rawMessage.message, message_tokens)
-    append_history_to_send(message_history, rawMessage, message_tokens, 'response')
+    appendHistoryToSend(message_history, rawMessage, message_tokens, 'response')
     print("")
     print(Fore.LIGHTRED_EX + "LAALA: " + message + Style.RESET_ALL)
     
