09ae1139f23e540473ab1624f0d7e3bf55a64b50
Sodra <sodra9000@gmail.com>
2023-03-12 17:07:19 -0700

    Token popper functionality restored.
    historyTokenManager class works.

--- laala.py
+++ laala.py
@@ -12,7 +12,7 @@ if "boring" in sys.argv:
     with open('boring_mode.txt', 'r') as file:
         system_desu = file.read().strip()
 if "big" in sys.argv:
-    with open('big_text.txt', 'r') as file:
+    with open('big_text.txt', 'r', encoding='utf-8') as file:
         system_desu = file.read().strip()
 else:
     with open('laala_prompt.txt', 'r') as file:
@@ -25,24 +25,56 @@ else:
 
 # Message History Class
 # Contains Message History for gpt context
+max_context_size = 4096
+# response size affects API request
+max_response_size = 500
+#max_history_size = max_context_size - max_response_size
 
-#class historyTokenManager:
-#    def __init__(self):
+class historyTokenManager:
+    def __init__(self):
+        print("bingle")
+        self.tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+    
+    def countTokens(self, prompt):
+        self.tokenList = self.tokenizer(prompt)
+        self.tokenCount = len(self.tokenList[0])
+        #print('Current tokenCount: ', self.tokenCount)
+        return self.tokenCount
+    
+    def maxTokenSize(self):
+        self.maxTokenSizeNum = max_context_size
+        self.maxTokenResponseSize = max_response_size
+        self.maxHistorySize = self.maxTokenSizeNum - self.maxTokenResponseSize
+        return self.maxHistorySize
 
+    #def countTotalTokens(self):
+    #    self.tokenHistorySum = sum()
+
+    def currentAmountOfTokens(self, x): #this returns the number of tokens in the history right now.
+        return sum(item[1] for item in x)
+
+    def popTokens(self, message_history): #return whole message_history, with tokens popped
+        #print(help(message_history))
+
+        #self.currentAmountOfTokens = sum(item[1] for item in message_history)
+        self.totalAllowedTokens = self.maxTokenSize()
+        while self.currentAmountOfTokens(message_history) > self.totalAllowedTokens:
+            #print("Message history is currently " + str(self.currentAmountOfTokens(message_history)) + ", popping.")
+            message_history.pop(1)
+        #print("Message history is currently " + str(self.currentAmountOfTokens(message_history)) + ", popping complete.")
+        return message_history
+
+#yo if you add "Answer as LAALA" to every last prompt it totally works, you can even remove it per history so it doesn't stay in context
 class MessageHistoryStore:
     def __init__(self):
         self.message_history = []
+        self.historyTokenManager = historyTokenManager()
 
     #INPUT: "USER" "HELLO"
     #OUTPUT: [{"role": "USER", "content": "HELLO"}, 1]
     def entryFormatter(self, prompt, messageSide):
         self.dictionaryCreator = {"role": messageSide, "content": prompt}
-        tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-        self.tokenList = tokenizer(prompt)
-        self.tokenCount = len(self.tokenList[0])
-        print('Current tokenCount: ', self.tokenCount)
-        #TODO: Straight up, just countToken(prompt) and add to the tuple. ezpz
-        self.tupleToEntry = (self.dictionaryCreator, self.tokenCount)
+        self.tupleToEntry = (self.dictionaryCreator, self.historyTokenManager.countTokens(prompt))
         return self.tupleToEntry
 
     def newEntry(self, prompt, messageSide):
@@ -54,10 +86,13 @@ class MessageHistoryStore:
         return self.historyDictionariesOnly
 
     def sendRequestToAPI(self):
+        #self.message_history = self.historyTokenManager.popTokens(self.message_history)
+        self.historyTokenManager.popTokens(self.message_history)
         self.messagesToSend = self.formattedHistoryForAPI()
         self.rawAPIResponse = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-        		messages = self.messagesToSend
+        		messages = self.messagesToSend,
+                max_tokens=max_response_size
             )
         ###Response
         self.AI_Response = self.rawAPIResponse.choices[0].message.content
