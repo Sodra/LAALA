c163cb3165c3fb0a95f1dcc7b71124a8a460a9d7
Sodra <sodra9000@gmail.com>
2023-04-23 18:44:00 -0700

    Initial discord rewrite of laala

--- laala.py
+++ laala.py
@@ -1,196 +1,86 @@
-#!/usr/bin/env python3
-
-import openai
-from colorama import init, Fore, Back, Style
-from typing import List
+import langchain
+import os
+import re
 import sys
-import tiktoken
-init()
 
+from langchain.prompts import (
+    ChatPromptTemplate, 
+    MessagesPlaceholder, 
+    SystemMessagePromptTemplate, 
+    HumanMessagePromptTemplate
+)
 
-#def debugMode():
-#elif "debug" in sys.argv:
-#    pass
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationTokenBufferMemory
 
-if "boring" in sys.argv:
-    with open('boring_mode.txt', 'r') as file:
-        system_desu = file.read().strip()
-    print("Boring Mode Activated")
-elif "big" in sys.argv:
-    with open('big_text.txt', 'r', encoding='utf-8') as file:
-        system_desu = file.read().strip()
-else:
-    with open('laala_prompt.txt', 'r') as file:
-        system_desu = file.read().strip()
+##############################################################
+##############################################################
+##############################################################
 
+channelIDsToListen = [1071518668896346162]
+pattern = r'[Ll][Aa][Aa][Ll][Aa]'
 
+with open('laala_prompt.txt', 'r') as file:
+        laala_prompt = file.read().strip()
 
-    
-
-#TODO make thie use python logging
-#TODO proper name logging, will attach to discord integration
-class chatLog:
-    def writeToLogBeginning(self):
-        with open('chat.log', 'a', encoding='utf-8') as file:
-            file.write(f'##### LAALA ONLINE c: #####\n\n')
-    
-    def writeToLog(self, role, prompt):
-        if role == "user":
-            role = "You"
-        elif role == "assistant":
-            role = "LAALA"
-        with open('chat.log', 'a', encoding='utf-8') as file:
-            file.write(f'{role}: {prompt}\n\n')
-    
-    def __init__(self):
-        self.writeToLogBeginning()
-
-# Message History Class
-# Contains Message History for gpt context
-max_context_size = 4096
-# response size affects API request
-max_response_size = 300
-#max_history_size = max_context_size - max_response_size
-
-class tokenizerClass:
-    def __init__(self):
-        self.tokenModel = tiktoken.encoding_for_model('gpt-3.5-turbo')
-
-    def tokenizer(self, prompt2):
-        self.splitIntoTokens = self.tokenModel.encode(prompt2)
-        #print(self.splitIntoTokens)
-        return len(self.splitIntoTokens)
-
-class historyTokenManager:
-    def __init__(self):
-        #print("bingle")
-        #self.tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-        self.tokenizer = tokenizerClass()
-    
-    def countTokens(self, prompt):
-        #self.tokenList = self.tokenizer(prompt)
-        #self.tokenCount = len(self.tokenList[0])
-        self.tokenCount = self.tokenizer.tokenizer(prompt)
-        #print('Current tokenCount: ', self.tokenCount)
-        return self.tokenCount
-    
-    def maxTokenSize(self):
-        self.maxTokenSizeNum = max_context_size
-        self.maxTokenResponseSize = max_response_size
-        self.maxHistorySize = self.maxTokenSizeNum - self.maxTokenResponseSize
-        return self.maxHistorySize
-
-    #def countTotalTokens(self):
-    #    self.tokenHistorySum = sum()
-
-    def currentAmountOfTokens(self, x): #this returns the number of tokens in the history right now.
-        return sum(item[1] for item in x)
-
-    def popTokens(self, message_history): #return whole message_history, with tokens popped
-        #print(help(message_history))
-
-        #self.currentAmountOfTokens = sum(item[1] for item in message_history)
-        self.totalAllowedTokens = self.maxTokenSize()
-        while self.currentAmountOfTokens(message_history) > self.totalAllowedTokens:
-            #print("Message history is currently " + str(self.currentAmountOfTokens(message_history)) + ", popping.")
-            message_history.pop(1)
-        #print("Message history is currently " + str(self.currentAmountOfTokens(message_history)) + ", popping complete.")
-        return message_history
-
-#yo if you add "Answer as LAALA" to every last prompt it totally works, you can even remove it per history so it doesn't stay in context
-class MessageHistoryStore:
-    def __init__(self):
-        self.message_history = []
-        self.historyTokenManager = historyTokenManager()
-        self.chatLog = chatLog()
-
-    #INPUT: "USER" "HELLO"
-    #OUTPUT: [{"role": "USER", "content": "HELLO"}, 1]
-    def entryFormatter(self, prompt, messageSide):
-        self.dictionaryCreator = {"role": messageSide, "content": prompt}
-        self.tupleToEntry = (self.dictionaryCreator, self.historyTokenManager.countTokens(prompt))
-        return self.tupleToEntry
-
-    def newEntry(self, prompt, messageSide):
-        addThisThingToHistory = self.entryFormatter(prompt, messageSide)
-        self.chatLog.writeToLog(messageSide, prompt)
-        self.message_history.append(addThisThingToHistory)
-
-    def formattedHistoryForAPI(self):
-        self.historyDictionariesOnly = [item[0] for item in self.message_history]
-        return self.historyDictionariesOnly
-
-    def sendRequestToAPI(self):
-        #self.message_history = self.historyTokenManager.popTokens(self.message_history)
-        self.historyTokenManager.popTokens(self.message_history)
-        self.messagesToSend = self.formattedHistoryForAPI()
-        #print("Sending this many tokens to the API: " + str(self.historyTokenManager.currentAmountOfTokens(self.message_history)))
-        self.rawAPIResponse = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-        		messages = self.messagesToSend,
-                max_tokens=max_response_size
-            )
-        ###Response
-        self.AI_Response = self.rawAPIResponse.choices[0].message.content
-        self.newEntry(self.AI_Response, "assistant")
-        return self.AI_Response
-
-    #####
-    #send request
-    #####
-
-    def receiveResponse(self):
-        self.AI_message = self.sendRequestToAPI()
-        return self.AI_message
-
-    def makeRequestToSend(self, prompt):
-        self.newEntry(prompt, "user")
-
-class LAALA_UI:
-    def printLAALA(self, x):
-        print(Fore.LIGHTRED_EX + "LAALA: " + x)
-        print("")
-
-    def askLAALA(self):
-        self.prompt = input(Fore.CYAN + "You: ")
-        print("")
-        return self.prompt
-
-    def convoLoop(self, MessageHistoryStore):
-        MessageHistoryStore.makeRequestToSend(self.askLAALA())
-        self.printLAALA(MessageHistoryStore.receiveResponse())
-        self.convoLoop(MessageHistoryStore)
-
-    def __init__(self, MessageHistoryStore):
-        MessageHistoryStore.makeRequestToSend(system_desu)
-        self.printLAALA(MessageHistoryStore.receiveResponse())
-        self.convoLoop(MessageHistoryStore)
-
+with open('discord.key', 'r') as file:
+        discord_bot_key = file.read().strip()
 
 with open('api.key', 'r') as file:
-    priTicket = file.read().strip()
-openai.api_key = str(priTicket)
+        os.environ["OPENAI_API_KEY"] = file.read().strip()
 
-def inputYourPrompt():
-    prompt = input(Fore.CYAN + "You: ")
-    return prompt
+##############################################################
+##############################################################
+##############################################################
 
+import discord
+from discord.ext import commands
+intents = discord.Intents.all()
+intents.members = True  # We need to enable the members intent explicitly
+bot = commands.Bot(command_prefix='!', intents=intents)
+guild = discord.Guild
+messages = []
 
+class discordIO:
+    def __init__(self):
+        pass
 
-print("## LAALA ONLINE c: ##\n")
+    async def send_message(self, message):
+        await self.message.channel.send
 
-MessageHistoryStore = MessageHistoryStore()
-LAALA = LAALA_UI(MessageHistoryStore)
+    @bot.event
+    async def on_message(self, message):
+        if message.channel.id in channelIDsToListen:
+            pass
+    
 
+##############################################################
+##############################################################
+##############################################################
 
-#MessageHistoryStore.newEntry("bingle", "user")
-'''while True:
-    thisIsYourPrompt = LAALA.askLAALA()
-    print("")
-    MessageHistoryStore.makeRequestToSend(thisIsYourPrompt)
-    print(Fore.LIGHTRED_EX + "LAALA: " + MessageHistoryStore.receiveResponse().lstrip())
-    print("")'''
-#print(MessageHistoryStore.receiveResponse())
-#print(MessageHistoryStore.message_history)
-#print(MessageHistoryStore.receiveResponse())
+class modelIO:
+    def __init__(self):
+        self.prompt = ChatPromptTemplate.from_messages([
+            SystemMessagePromptTemplate.from_template(laala_prompt),
+            MessagesPlaceholder(variable_name="history"),
+            HumanMessagePromptTemplate.from_template("{input}")
+        ])
 
+        self.llm = ChatOpenAI(temperature=1.1)
+        self.memory = ConversationTokenBufferMemory(llm=self.llm, max_token_limit=1024, return_messages=True)
+        self.conversation = ConversationChain(
+            prompt=self.prompt,
+            llm=self.llm, 
+            verbose=True, 
+            memory=self.memory
+        )
+    
+    def remove_shim_from_history_before_sending(self):
+         #replace the messagesPlaceholder in the chat prompt template?
+         pass
+    
+    def sendToModel(self, text):
+         return self.conversation.predict(text)
+    
+aipart = modelIO()
