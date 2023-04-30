785e403354d80135bd608e753032e182fbc88a84
Sodra <sodra9000@gmail.com>
2023-04-29 21:04:06 -0700

    Initial successful running code!

--- laala.py
+++ laala.py
@@ -15,12 +15,13 @@ from langchain.memory import ConversationTokenBufferMemory
 ##############################################################
 ##############################################################
 
-channelIDsToListen = [1071518668896346162] #bingle channel
+channelIDsToListen = [1071518668896346162, 1083806446094917682] #bingle channel
 guildIDsToListen = [274453805792362507] #dank emojis
 adminRoleIDsToListen = [483366907777384460, 483366907777384461] #consul
 adminUserIDsToisten = [237773697811742720] #soda
+ALL_CHANNELS = False
 
-pattern = r'[Ll][Aa][Ll][Aa]'
+pattern = r'[Ll][Aa][Aa][Ll][Aa]'
 
 with open('laala_prompt.txt', 'r') as file:
         laala_prompt = file.read().strip()
@@ -73,13 +74,28 @@ class modelIO:
          #replace the messagesPlaceholder in the chat prompt template?
          pass
     
-    def sendToModel(self, text):
-         return self.conversation.predict(text)
+    def remove_labels(self, rawLaalaResponse):
+        #Removes laala: at the beginning, for shim use
+        self.label = r'[Ll][Aa][Aa][Ll][Aa]:'
+        self.label2 = r'[Rr][Ee][Ss][Pp][Oo][Nn][Ss][Ee]: '
+        self.cleanedLaalaResponse = re.sub(pattern, '', rawLaalaResponse)
+        self.cleanedLaalaResponse = re.sub(pattern, '', self.cleanedLaalaResponse)
+        return self.cleanedLaalaResponse
+
+    def sendToModel(self, username, messagecontent):
+        #Send to remove_label cleaner to remove laala: at the beginning
+        cleanedMessageContent = self.remove_labels(messagecontent)
+
+        #Send to the model directly
+        self.currentMessage = f"{username}: {cleanedMessageContent}"
+        return self.conversation.predict(input=self.currentMessage)
     
 aipart = modelIO()
 print("sup")
 
-
+##############################################################
+##############################################################
+##############################################################
 
 @bot.event
 async def on_message(message):
@@ -91,7 +107,7 @@ async def on_message(message):
         if message.guild.id in guildIDsToListen:
 
             #If allowed in the channel scope
-            if message.channel.id in channelIDsToListen:
+            if message.channel.id in channelIDsToListen or ALL_CHANNELS:
 
                 #If admin role is found in user who sent message
                 try:
@@ -112,22 +128,21 @@ async def on_message(message):
                     
                     #If message is a reply to laala
                     if repliedMessage.author.id == bot.user.id:
-                        await message.reply("wow dat me")
+                        await message.channel.typing()
+                        laalaResponse = aipart.sendToModel(message.author.display_name, message.content)
+                        await message.reply(laalaResponse)
                     
                 #If "laala" is found in the message content
-                if re.search(pattern, message.content):
+                elif re.search(pattern, message.content):
                     await message.channel.typing()
-                    f"{message.author.display_name}: {message.content}"
-                    modelIO.sendToModel()
-                    await message.reply(input)
+                    laalaResponse = aipart.sendToModel(message.author.display_name, message.content)
+                    await message.reply(laalaResponse)
 
 @bot.event
 async def on_ready():
     bingle = bot.get_channel(1071518668896346162)
     await bingle.send("**## LAALA ONLINE c: ##***")
     #await bot.change_presence(activity=discord.Game(name="## LAALA ONLINE c: ##"))
-
-
 
 bot.run(discord_bot_key)
 
